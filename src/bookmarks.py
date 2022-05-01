c15d4a41a1321482e8e9b2a03730019dcf9ba6a2
Himanshu Suthar <h9714348433@gmail.com>
2022-05-01 19:23:44 +0530

    Retrieve one bookmark

--- src/bookmarks.py
+++ src/bookmarks.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from src.database import Bookmark, db
 import validators
-from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_409_CONFLICT
+from src.constants.http_status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_409_CONFLICT
 from flask_jwt_extended import get_jwt_identity, jwt_required
 
 
@@ -67,3 +67,25 @@ def handle_bookmarks():
         }
 
         return jsonify({"data": data, "meta": meta}), HTTP_200_OK
+
+
+@bookmarks.get("/<int:id>")
+@jwt_required()
+def get_bookmark(id):
+    current_user = get_jwt_identity()
+    bookmark = Bookmark.query.filter_by(user_id=current_user, id=id).first()
+
+    if not bookmark:
+        return jsonify({
+            "message": "Item not found!"
+        }), HTTP_404_NOT_FOUND
+
+    return jsonify({
+        "id": bookmark.id,
+        "url": bookmark.url, 
+        "short_url": bookmark.short_url,
+        "visits": bookmark.visits,
+        "body": bookmark.body,
+        "created_at": bookmark.created_at,
+        "updated_at": bookmark.updated_at
+    })
